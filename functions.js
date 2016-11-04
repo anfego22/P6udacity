525c809c1277fb467e47fbcd88abeb668c289571
Andres <anfego22@gmail.com>
2016-11-03 20:22:07 -0500

    Add description

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+
 function printMap(){
 	diasConver = {Mon: 'lunes', Tue: 'martes',
 				  Wen: 'miércoles', Thu:'jueves',
@@ -62,5 +63,165 @@ function playAll(dur){
 		if(day_index >= days.length){
 			clearInterval(playS);
 		}
-	}, dur*7 + 50);
+	}, dur*7);
 }
+
+// CODE
+
+var padDia = 35;
+var margin = {top: 10, right: 10,
+			  bottom: 10, left: 10};
+var height = 900 - margin.top - margin.bottom;
+var width = 750 - margin.right - margin.left;
+var svg = d3.select('#svgHere')
+	.append('svg')
+	.attr('width', width + margin.right +
+		  margin.left)
+	.attr('height', height + margin.top +
+		  margin.bottom)
+	.append('g');
+var ls_w = 20, ls_h = 20;
+var diaGlob = 'Mon';
+var horaGlob = '[0-4)';
+var diasConver = {Mon: 'lunes', Tue: 'martes',
+				  Wen: 'miércoles', Thu:'jueves',
+				  Fri: 'viernes', Sat: 'sábado',
+				  Sun: 'domingo'}
+var HoraInt = ['[0-4)', '[4-8)', '[8-12)',
+			   '[12- 16)', '[16-20)', '[20-24)']
+var hora = svg.selectAll('g')
+	.data(HoraInt)
+	.enter()
+	.append('g')
+	.attr('transform', function(d, i){
+		return 'translate(150, ' +
+			(i*20 + padDia) +
+			')';
+	});
+d3.select('svg')
+	.append('text')
+	.attr('x', 150)
+	.attr('y', 25)
+	.attr('class', 'title')
+	.text('Time of Day (Hours)');
+d3.select('svg')
+	.append('text')
+	.attr('x', 10)
+	.attr('y', 25)
+	.attr('class', 'title')
+	.text('Day of Week');
+hora.append('rect')
+	.attr("width", 110)
+	.attr("height", ls_h)
+	.attr('class', 'rectangleH')
+	.attr('dy', '.35em')
+	.attr('id', function(d, i){
+		return 'Hora' + i;})
+	.on('click', function(d){
+		d3.selectAll('rect.rectangleH').transition()
+			.duration(300)
+			.style("fill", "#333");
+		d3.select(this).transition().duration(300)
+			.style("fill", "#FFD700");
+		horaGlob = d;
+		printMap();
+	});
+hora.append('text')
+	.attr('x', 20)
+	.attr('y', 15)
+	.attr('width', ls_w)
+	.attr('heigth', ls_h)
+	.attr('fill', 'white')
+	.text(function(d) { 
+		return d;});
+var diasSem = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri',
+			   'Sat', 'Sun'];
+var dias = svg.selectAll('g.rectangle')
+	.data(diasSem)
+	.enter()
+	.append('g')
+	.attr('transform', function(d, i){
+		return 'translate(0, ' +
+			(i*20 + padDia) +
+			')';
+	});
+dias.append('rect')
+	.attr("width", 110)
+	.attr("height", ls_h)
+	.attr('class', 'rectangle')
+	.attr('id', function(d){
+		return d;})
+	.attr('dy', '.35em')
+	.on('click', function(d){
+		d3.selectAll('rect.rectangle').transition()
+			.duration(300)
+			.style("fill", "#333");
+		d3.select(this).transition().duration(300)
+			.style("fill", "#FFD700");
+		diaGlob = d;
+		printMap();
+	});
+dias.append('text')
+	.attr('x', 20)
+	.attr('y', 15)
+	.attr('width', ls_w)
+	.attr('heigth', ls_h)
+	.attr('fill', 'white')
+	.text(function(d) { 
+		return d;});
+var homRan = [5, 10, 20, 50, 100, 150, 200];
+var legend_labels = ["< 5", "(5 -10]",
+					 "(10 - 20]", "(20 - 50]",
+					 "(50 - 100]", "(100 - 150]",
+					 "> 200"];
+var colrRan = ['#ffffe0','#ffd59b','#ffa474',
+			   '#f47461', '#db4551','#b81b34',
+			   '#8b0000'];
+var colors = d3.scale.threshold()
+	.domain(homRan)
+	.range(colrRan);
+var legend = svg.selectAll("g.legend")
+	.data(homRan)
+	.enter().append("g")
+	.attr("class", "legend");
+var ls_w = 20, ls_h = 20;
+legend.append("rect")
+	.attr("x", width - 150)
+	.attr("y", function(d, i){
+		return (i*ls_h) + 30;})
+	.attr("width", ls_w)
+	.attr("height", ls_h)
+	.style("fill", function(d, i) {
+		return colors(d); })
+	.style("opacity", 0.8);
+legend.append("text")
+	.attr("x", width-120)
+	.attr("y", function(d, i){
+		return (i*ls_h) + 49;})
+	.text(function(d, i){
+		return legend_labels[i]; });
+d3.select('svg')
+	.append('text')
+	.attr('x', width -150)
+	.attr('y', 25)
+	.attr('class', 'title')
+	.text('Homicides (#)');
+var play = d3.select('svg')
+	.append('g')
+	.attr('transform', 'translate(0, 200)');
+play.append('rect')
+	.attr("width", 110)
+	.attr("height", ls_h)
+	.attr('class', 'rectangleP')
+	.on('click', function(d){
+		playMap(1000);
+	});
+play.append('text')
+	.attr('x', 20)
+	.attr('y', 15)
+	.attr('fill', 'white')
+	.attr('width', ls_w)
+	.attr('heigth', ls_h)
+	.text('Play');
+d3.json('localidades_bogota.geojson', draw);
+playAll(1000);
