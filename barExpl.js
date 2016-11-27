fc7bb1f4f0862ac5eb18f9bc0a8adac97873167b
Andres <anfego22@gmail.com>
2016-11-27 13:38:04 -0500

    Flip x and y axes in the bar graph

--- barExpl.js
+++ barExpl.js
@@ -1,29 +1,28 @@
-var marginB = {top: 10, right: 10,
-			  bottom: 25, left: 10};
+var marginB = {top: 30, right: 20,
+			  bottom: 25, left: 150};
 var heightB = 500 - margin.top - margin.bottom;
-var widthB = 850 - margin.right - margin.left;
+var widthB = 720 - margin.right - margin.left;
 
 
 // Create scales for the bar graph
-var x0 = d3.scale.ordinal()
-    .rangeRoundBands([0, widthB], .1);
-var x1 = d3.scale.ordinal();
-var y = d3.scale.linear()
-    .range([heightB, 0]);
+var y0 = d3.scale.ordinal()
+    .rangeRoundBands([0, heightB], .1);
+var y1 = d3.scale.ordinal();
+var x = d3.scale.linear()
+    .range([0, widthB]);
 
-// Scale for 19 localidades!!
 var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56",
-			"#d0743c"]);
+    .range(["#0000FF", "#DC143C", "#006400",
+			"#FF8C00", "#FFD700", "#00FF00"]);
 
 // Making the line axis of the bar
-var xAxis = d3.svg.axis()
-    .scale(x0)
-    .orient("bottom");
-
 var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
+    .scale(y0)
+    .orient("left");
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
     .tickFormat(d3.format(".2s"));
 
 // Create svg element under the svgBar tag
@@ -43,7 +42,6 @@ d3.csv("totalHomW.csv", function(error, data) {
 	if (error) throw error;
 	// This filter is done already in totalHomW.csv
 	//data = data.filter(function(d){ return d.TotalHom > 93;})
-	debugger;
 	var LocalidName = d3.keys(data[0]).filter(function(k){
 		return k != 'Localidad';});
 	
@@ -51,43 +49,46 @@ d3.csv("totalHomW.csv", function(error, data) {
 		d.ages = LocalidName.map(function(name) {
 			return {name: name, value: +d[name]}; });
 	});
-	x0.domain(data.map(function(d) { return d.Localidad; }));
-	x1.domain(LocalidName).rangeRoundBands([0, x0.rangeBand()]);
-	y.domain([0, d3.max(data, function(d) {
+	y0.domain(data.map(function(d) { return d.Localidad; }));
+	y1.domain(LocalidName).rangeRoundBands([0, y0.rangeBand()]);
+	x.domain([0, d3.max(data, function(d) {
 		return d3.max(d.ages, function(d) { return d.value; }); })]);
 	
 	svgBar.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + heightB + ")")
 		.call(xAxis);
-
+	
 	svgBar.append("g")
 		.attr("class", "y axis")
 		.call(yAxis)
-		.append("text")
-		.attr("transform", "rotate(-90)")
 		.attr("y", 6)
 		.attr("dy", ".71em")
-		.style("text-anchor", "end")
-		.text("Homicides");
+		.style("text-anchor", "end");
+
+	svgBar.append('text')
+		.attr('class', 'title')
+		.attr('x', 0)
+		.attr('y', -15)
+		.text('Homicides above the 3rd quantile of the sample');
 
 	var state = svgBar.selectAll(".state")
 		.data(data)
 		.enter().append("g")
 		.attr("class", "g")
 		.attr("transform", function(d) {
-			return "translate(" + x0(d.Localidad) + ",0)"; });
+			return "translate(0," + y0(d.Localidad) + ")"; });
 
+	debugger;
 	state.selectAll("rect")
 		.data(function(d) { return d.ages; })
 		.enter().append("rect")
-		.attr("width", x1.rangeBand())
-		.attr("x", function(d) { return x1(d.name); })
-		.attr("y", function(d) { return y(d.value); })
-		.attr("height", function(d) { return heightB - y(d.value); })
+		.attr("height", y1.rangeBand())
+		.attr("y", function(d) { return y1(d.name); })
+		//.attr("x", function(d) { return x(d.value); })
+		.attr("width", function(d) { return x(d.value); })
 		.style("fill", function(d) { return color(d.name); });
 
-	
 	var legend = svgBar.selectAll(".legend")
 		.data(LocalidName.slice().reverse())
 		.enter().append("g")
@@ -96,15 +97,16 @@ d3.csv("totalHomW.csv", function(error, data) {
 			return "translate(0," + i * 20 + ")"; });
 
 	legend.append("rect")
-		.attr("x", width - 18)
+		.attr("x", widthB - 18)
 		.attr("width", 18)
 		.attr("height", 18)
 		.style("fill", color);
 
 	legend.append("text")
-		.attr("x", width - 24)
+		.attr("x", widthB - 24)
 		.attr("y", 9)
 		.attr("dy", ".35em")
 		.style("text-anchor", "end")
 		.text(function(d) { return d; });
+	
 });
